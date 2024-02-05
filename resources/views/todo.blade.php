c98bdf5013501cf2461bd5f6e8432a08a33ddec1
OzzyCosmo <oscarforbes821@gmail.com>
2024-02-05 22:07:05 +0000

    Update Todo List

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -4,8 +4,6 @@
 
 - Integrate ChatGPT into the site 
 <br>
-- Get cheap domain from IONOS, then host site
-<br>
 - Eventually make a way to let ChatGPT edit the site? Could make the output of ChatGPT detectable by a script that then changes the site, tell ChatGPT what it can do
 <br>
 - Find vulnerabilities
