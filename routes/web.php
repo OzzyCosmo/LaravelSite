0f92e925d8e57296abbde7276dcbdb6f05560ecc
OzzyCosmo <oscarforbes821@gmail.com>
2024-02-16 20:19:35 +0000

    LegionManager page
    Not fully made

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\OtherStuffController;
+use App\Http\Controllers\LegionManagerController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
@@ -21,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/otherstuff', [OtherStuffController::class, 'index'])->name('other-stuff');
+
+Route::get('/legion-manager', [LegionManagerController::class, 'index'])->name('legion-manager');
 
 Route::get('/todo', [TodoController::class, 'index'])->name('todo');
 
